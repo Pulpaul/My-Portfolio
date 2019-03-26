e76d80ea17f713f7b19dfe8006fef45feaf0ec91
Pulpaul <43722895+Pulpaul@users.noreply.github.com>
2019-03-26 12:57:21 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136790747-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-136790747-1');
+	</script>
+	<title>Portfolio</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="google-site-verification" content="BEkKJaUWtyTLmdg-SvKRjL2Rn4KF_oajC4JGyhFFgig" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+	<meta name="copyright" content="This portfolio is created by Paul Andrei T. Dela Peña" />
+	<meta name="description" content="This is my portfolio" />
+	<meta name="keywords" content="paul-myport, paul myport, paul my portfolio" />
+	<meta name="robots" content="index,follow" />
+	<meta name="DC.title" content="The Portfolio for my client" />
+	<link rel="icon" type="image/png" href="assets/img/ligo.png">
+	<link rel="stylesheet" type="text/css" href="assets/css/material-kit.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>  
+
+	<div class="lagayan">
+	  <img id="backg" src="assets/img/codes.jpg">
+	  <div class="logo" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="3000"><img class="lugo" src="assets/img/try.png"></div>
+	  <span class="center" data-aos="zoom-in" data-aos-duration="1500"><b>"CODE WITH PASSION"</b></span>
+	  <a href="portfolio.html" id="btn_con" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="3000"> CONTINUE </a> 
+	</div> 
+
+	<script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+	<script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+	<script src="assets/js/material-kit.min.js" type="text/javascript"></script>
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script>
+	  AOS.init();
+	</script>
+</body>
+</html>
